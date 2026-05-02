d33733fa9abe957a96689b48caad8fab4c7ce285
Migueel0 <miggaller@alum.us.es>
2026-05-02 15:48:12 +0200

    feat:Add mcp filesystem wrapper

---
+++ mcp-filesystem/wrapper.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const { spawn } = require('child_process');
+const readline = require('readline');
+
+const app = express();
+app.use(express.json());
+
+
+const mcpCommand = 'npx';
+
+const mcpArgs = [
+    '-y', 
+    '@modelcontextprotocol/server-filesystem', 
+    '/mnt/desktop', 
+    '/mnt/documentos-prueba'
+];
+
+const mcpProcess = spawn(mcpCommand, mcpArgs,{ shell: true });
+let sseResponse = null;
+
+const rl = readline.createInterface({
+    input: mcpProcess.stdout,
+    terminal: false
+});
+
+rl.on('line', (line) => {
+    if (sseResponse) {
+        sseResponse.write(`event: message\ndata: ${line}\n\n`);
+    }
+});
+
+mcpProcess.stderr.on('data', (data) => {
+    console.error(`[Filesystem MCP Log]: ${data.toString()}`);
+});
+
+
+app.get('/sse', (req, res) => {    
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    sseResponse = res;
+    
+    res.write(`event: endpoint\ndata: /messages\n\n`);
+});
+
+app.post('/messages', (req, res) => {
+    const message = JSON.stringify(req.body);
+    mcpProcess.stdin.write(message + '\n');
+    res.status(200).send('OK');
+});
+
+
+const PORT = 8001;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Filesystem server listening in port: ${PORT}`);
+});
